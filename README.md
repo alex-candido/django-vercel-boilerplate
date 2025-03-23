48f3b39a0fa51f46558d3f979f2f1a0aaec810f1
alex-candido <alex.candido.tec@gmail.com>
2025-03-23 19:24:56 -0300

    gerado requirements

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 - pdm init
 - pdm add django
 - pdm run django-admin startproject src
-
+- pdm export -f requirements --without-hashes > requirements.txt
--- src/django_app/settings.py
+++ src/django_app/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'django_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [os.path.join(BASE_DIR,'templates')],
+        'DIRS':  [os.path.join(BASE_DIR,'src/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
